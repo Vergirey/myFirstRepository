119ba2a1a918b722cf8386e2f9c0ae39cb3a7f73
Vergirey <sergey_v74@icloud.com>
2017-07-02 18:05:27 +0300

    All

--- AlertController/AlertController/ViewController.swift
+++ AlertController/AlertController/ViewController.swift
@@ -1,25 +1,61 @@
-//
-//  ViewController.swift
-//  AlertController
-//
-//  Created by Julia Kolbina on 02.07.17.
-//  Copyright © 2017 Sergey Volkov. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - VCLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
+    
+    var tempProperty = UIAlertController(title: "MyTitle", message: "MyMessage", preferredStyle: .actionSheet)
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - Actions
+    @IBAction func showAlert(_ sender: UIButton)
+    {
+        let alertViewController = UIAlertController(title: "MyTitle", message: "MyMessage", preferredStyle: .alert)
+        
+        let handler = { (action:UIAlertAction) -> Void in
+            print("\(String(describing: action.title))")
+        }
+        
+        let okAction = UIAlertAction(title: "MY - OK", style: .default, handler: handler)
+        alertViewController.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "MY - CANCEL", style: .cancel, handler: handler)
+        alertViewController.addAction(cancelAction)
+        
+        self.present(alertViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction func showActionSheet(_ sender: UIButton)
+    {
+        let alertViewController = UIAlertController(title: "MyTitle", message: "MyMessage", preferredStyle: .actionSheet)
+        
+        let handler = { [weak  self] (action:UIAlertAction) -> Void in
+            self?.customPrint(string: action.title)
+        }
+        
+        let okAction = UIAlertAction(title: "MY - OK", style: .default, handler: handler)
+        alertViewController.addAction(okAction)
+        
+        tempProperty.addAction(okAction)
+        
+        let cancelAction = UIAlertAction(title: "MY - CANCEL", style: .cancel, handler: handler)
+        alertViewController.addAction(cancelAction)
+        
+        self.present(alertViewController, animated: true, completion: nil)
+    }
+    
+    func customPrint(string:String?)
+    {
+        print("Custom print - \(String(describing: string))")
+    }
 }
-
