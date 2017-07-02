7da913d299bd5dc803f15d5a705b47f24ecc7f0f
Vergirey <sergey_v74@icloud.com>
2017-07-02 18:33:37 +0300

    friends & enemies Refactoring;

--- AlertController/AlertController/ViewController.swift
+++ AlertController/AlertController/ViewController.swift
@@ -1,3 +1,12 @@
+
+//
+//  AppDelegate.swift
+//  AlertController
+//
+//  Created by Julia Kolbina on 02.07.17.
+//  Copyright © 2017 Sergey Volkov. All rights reserved.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -17,19 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    let friends = ["Leo", "Stive", "Will"]
+    let enemies = ["enemyie1", "enemyie2", "enemyie3"]
+    
+    
     // MARK: - Actions
     @IBAction func showAlert(_ sender: UIButton)
     {
         let alertViewController = UIAlertController(title: "MyTitle", message: "MyMessage", preferredStyle: .alert)
         
-        let handler = { (action:UIAlertAction) -> Void in
-            print("\(String(describing: action.title))")
+        let wraper = {(array:[String]) -> (UIAlertAction) -> Void in
+            return { (action:UIAlertAction) -> Void in
+                print("\(String(describing: action.title))")
+                print("\(array)")
+            }
         }
-        
-        let okAction = UIAlertAction(title: "MY - OK", style: .default, handler: handler)
+
+        let okAction = UIAlertAction(title: "Show Friends", style: .default, handler: wraper(friends))
         alertViewController.addAction(okAction)
         
-        let cancelAction = UIAlertAction(title: "MY - CANCEL", style: .cancel, handler: handler)
+        let cancelAction = UIAlertAction(title: "Shoq Enemies", style: .cancel, handler: wraper(enemies))
         alertViewController.addAction(cancelAction)
         
         self.present(alertViewController, animated: true, completion: nil)
